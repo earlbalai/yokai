b085f3ae94e086b8daab1e86cc3021cc43068d5b
Earl Balai Jr <earl@kiwi.io>
2015-12-22 20:18:05 -1000

    New relay texture and added animations

--- objects/wired/relay/relay.lua
+++ objects/wired/relay/relay.lua
@@ -10,15 +10,18 @@ function output(state)
   if state ~= storage.state then
     storage.state = state
     entity.setAllOutboundNodes(state)
+    if state then
+      entity.setAnimationState("relayState", "on")
+    else
+      entity.setAnimationState("relayState", "off")
+    end
   end
 end
 
 function update(dt)
   if entity.isInboundNodeConnected(0) and entity.getInboundNodeLevel(0) then
     output(true)
-    -- entity.setAllOutboundNodes(true)
   else
     output(false)
-    -- entity.setAllOutboundNodes(false)
   end
 end
